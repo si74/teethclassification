66530418d6a9908f260187d1835bd9840cce7ed1
yct4 <yct4@cornell.edu>
2019-07-08 18:21:09 -0400

    training works

--- classifier.py
+++ classifier.py
@@ -5,14 +5,14 @@ from keras.layers import Activation, Dropout, Flatten, Dense
 from keras import backend as K
 
 # Training data directory
-good_data_dir = 'training_images'
+train_data_dir = 'training_images'
 
 # Test data
-test_data_dir = 'test_images'
+validation_data_dir = 'test_images'
 
 # Total samples (below x 2 for good and bad)
-training_samples = 222
-test_samples = 20
+nb_train_samples = 222
+nb_validation_samples = 20
 
 
 # Define epoch and batch size (what exactly is this?)
@@ -26,3 +26,58 @@ if K.image_data_format() == 'channels_first':
     input_shape = (3, img_width, img_height)
 else:
     input_shape = (img_width, img_height, 3)
+
+model = Sequential() 
+model.add(Conv2D(32, (2, 2), input_shape=input_shape)) 
+model.add(Activation('relu')) 
+model.add(MaxPooling2D(pool_size=(2, 2))) 
+  
+model.add(Conv2D(32, (2, 2))) 
+model.add(Activation('relu')) 
+model.add(MaxPooling2D(pool_size=(2, 2))) 
+  
+model.add(Conv2D(64, (2, 2))) 
+model.add(Activation('relu')) 
+model.add(MaxPooling2D(pool_size=(2, 2))) 
+  
+model.add(Flatten()) 
+model.add(Dense(64)) 
+model.add(Activation('relu')) 
+model.add(Dropout(0.5)) 
+model.add(Dense(1)) 
+model.add(Activation('sigmoid'))
+
+
+model.compile(loss='binary_crossentropy', 
+              optimizer='rmsprop', 
+              metrics=['accuracy']) 
+
+# Load and label data
+train_datagen = ImageDataGenerator( 
+    rescale=1. / 255, 
+    shear_range=0.2, 
+    zoom_range=0.2, 
+    horizontal_flip=True) 
+  
+test_datagen = ImageDataGenerator(rescale=1. / 255) 
+  
+train_generator = train_datagen.flow_from_directory( 
+    train_data_dir, 
+    target_size=(img_width, img_height), 
+    batch_size=batch_size, 
+    class_mode='binary') 
+  
+validation_generator = test_datagen.flow_from_directory( 
+    validation_data_dir, 
+    target_size=(img_width, img_height), 
+    batch_size=batch_size, 
+    class_mode='binary') 
+  
+model.fit_generator( 
+    train_generator, 
+    steps_per_epoch=nb_train_samples // batch_size, 
+    epochs=epochs, 
+    validation_data=validation_generator, 
+    validation_steps=nb_validation_samples // batch_size) 
+
+model.save_weights('teeth_classifier.h5') 
